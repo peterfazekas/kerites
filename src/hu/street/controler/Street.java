6b41245eeda4cac7042b8203f7d54047be34317d
Peter_Fazekas <v-pfazekas@hotels.com>
2019-03-15 16:13:10 +0100

    Street update

--- src/hu/street/controler/Street.java
+++ src/hu/street/controler/Street.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
